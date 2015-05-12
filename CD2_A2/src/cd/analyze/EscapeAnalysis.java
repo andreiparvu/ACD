1ec50365d814fac392202f24b910341d276d28a2
Sebastian Wicki <gandro@gmx.net>
2015-05-12 11:11:03 +0200

    Refactor AliasSet into file

--- CD2_A2/src/cd/analyze/EscapeAnalysis.java
+++ CD2_A2/src/cd/analyze/EscapeAnalysis.java
@@ -1,12 +1,8 @@
 package cd.analyze;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -35,155 +31,9 @@ import cd.ir.BasicBlock;
 import cd.ir.Phi;
 import cd.ir.Symbol.ClassSymbol;
 import cd.ir.Symbol.MethodSymbol;
-import cd.ir.Symbol.TypeSymbol;
 import cd.ir.Symbol.VariableSymbol;
 
 public class EscapeAnalysis {
-
-	static class AliasSet {
-		static class AliasSetData {
-			private boolean escapes = false;
-			private final Map<String, AliasSet> fieldMap = new HashMap<>();
-		}
-		
-		private AliasSetData ref = new AliasSetData();
-		
-		public static AliasSet BOTTOM = new AliasSet();
-		static { BOTTOM.ref = null;	}
-
-		public static AliasSet forType(TypeSymbol type) {
-			if (type.isReferenceType()) {
-				return new AliasSet();
-			}
-			return BOTTOM;
-		}
-
-		void unify(AliasSet other) {
-			if (this.ref == other.ref) return;
-
-			Map<String, AliasSet> thisFields = this.ref.fieldMap;
-			Map<String, AliasSet> otherFields = other.ref.fieldMap;
-
-			this.ref.escapes |= other.ref.escapes;
-			other.ref = this.ref;
-
-			Set<String> fieldUnion = new HashSet<>(thisFields.keySet());
-			fieldUnion.addAll(otherFields.keySet());
-
-			for (String field : fieldUnion) {
-				AliasSet thisSet = thisFields.get(field);
-				AliasSet otherSet = otherFields.get(field);
-				if (thisSet != null && otherSet != null) {
-					// field in both maps, unify them
-					//thisSet.setEscapes(this.ref.escapes);
-					thisSet.ref.escapes |= this.ref.escapes;
-					thisSet.unify(otherSet);
-				} else if (thisSet == null) {
-					// missing in this
-					//otherSet.setEscapes(this.ref.escapes);
-					otherSet.ref.escapes |= this.ref.escapes;
-					thisFields.put(field, otherSet);
-				}
-				// we don't care if otherSet is null, `other` will be deleted
-			}
-
-			// `this` is the unified alias set
-		}
-		
-		public void unifyEscapes(AliasSet other) {
-			if (this.ref == other.ref || ref.escapes == other.ref.escapes) return;
-			this.ref.escapes |= other.ref.escapes;
-
-			Map<String, AliasSet> thisFields = this.ref.fieldMap;
-			Map<String, AliasSet> otherFields = other.ref.fieldMap;
-
-			Set<String> intersection = new HashSet<>(thisFields.keySet());
-			intersection.retainAll(otherFields.keySet());
-
-			for (String field : intersection) {
-				thisFields.get(field).unifyEscapes(otherFields.get(field));
-			}
-		}
-		
-		public AliasSet deepCopy() {
-			if (this.isBottom()) return BOTTOM;
-			
-			AliasSet copy = new AliasSet();
-			copy.ref.escapes = this.ref.escapes;
-			for (Entry<String, AliasSet> entry : ref.fieldMap.entrySet()) {
-				AliasSet sub = entry.getValue();
-				if (sub == this) {
-					sub = copy;
-				} else {
-					sub = sub.deepCopy();
-				}
-					// TODO this is probably not enough to deal with selfcontaining sets
-				copy.ref.fieldMap.put(entry.getKey(), sub);
-			}
-			return copy;
-		}
-		
-		public boolean isBottom() {
-			return this.ref == null;
-		}
-
-		public AliasSet fieldMap(String key) {
-			if (!this.ref.fieldMap.containsKey(key)) {
-				this.ref.fieldMap.put(key, new AliasSet());
-			}
-			return this.ref.fieldMap.get(key);
-		}
-		
-		public boolean escapes() {
-			return this.ref == null ? false : this.ref.escapes;
-		}
-		
-		public void setEscapes(boolean value) {
-			setEscapeRecursive(value, new HashSet<AliasSet>());
-		}
-		
-		private void setEscapeRecursive(boolean value, Set<AliasSet> marked) {
-			this.ref.escapes = value;
-			marked.add(this);
-			for (AliasSet child : this.ref.fieldMap.values()) {
-				if (!marked.contains(child)) {
-					child.setEscapeRecursive(value, marked);
-				}
-			}
-		}
-		
-		@Override
-		public String toString() {
-			StringWriter out = new StringWriter();
-			toStringRecursive(new PrintWriter(out), new HashSet<AliasSet>());
-			return out.toString();
-		}
-		
-		private void toStringRecursive(PrintWriter out, Set<AliasSet> marked) {
-			marked.add(this);
-			if (isBottom()) {
-				out.print("⊥");
-			} else {
-				out.format("<%s, {", ref.escapes);
-				Iterator<Entry<String, AliasSet>> iter = ref.fieldMap.entrySet().iterator();
-				while (iter.hasNext()) {
-					Entry<String, AliasSet> entry = iter.next();
-					out.format("%s:", entry.getKey());
-					AliasSet child = entry.getValue();
-					if (!marked.contains(child)) {
-						child.toStringRecursive(out, marked);
-					} else {
-						out.print("<..>");
-					}
-
-					if (iter.hasNext()) {
-						out.print(", ");
-					}
-				}
-				out.print("}>");
-			}
-		}
-	}
 
 	class AliasContext {
 		public final AliasSet receiver;
@@ -252,8 +102,6 @@ public class EscapeAnalysis {
 	private Map<Ast, AliasContext> siteContexts;
 	private CallGraph callGraph;
 	private Map<Ast, Boolean> multiSites;
-	private Map<Expr, AliasSet> exprSet;
-
 
 	public EscapeAnalysis(Main main) {
 		this.main = main;
@@ -279,7 +127,6 @@ public class EscapeAnalysis {
 		//// Phase 2 ////
 		methodContexts = new HashMap<MethodSymbol, AliasContext>();
 		siteContexts = new HashMap<Ast, AliasContext>();
-		exprSet = new HashMap<Expr, AliasSet>();
 
 		// traverse SCC methods in bottom-up topological order
 		analyzeBottomUp();
@@ -422,10 +269,9 @@ public class EscapeAnalysis {
 		}
 
 		@Override
-		public AliasSet visit(Expr ast, Void arg) {
-			AliasSet set = super.visit(ast, arg);
-			exprSet.put(ast, set);
-			return set;
+		public AliasSet visit(Expr expr, Void arg) {
+			expr.aliasSet = super.visit(expr, arg);
+			return expr.aliasSet;
 		}
 
 		@Override
